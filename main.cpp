3c5a3b00e4541b2826761737a244d38950a89cf3
Junior Maksim <mkostin531@gmail.com>
2024-10-25 00:30:17 +0200

    Внесение изменений в структуру кода

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main() {
+    int a, b;
+    cout << "Введите первое число : ";
+    cin >> a;
+    cout << "Введите второе число : ";
+    cin >> b;
