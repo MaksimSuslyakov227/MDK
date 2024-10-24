34677cf43bd07cb57072c4342f2b7c7092fa6245
Junior Maksim <mkostin531@gmail.com>
2024-10-25 01:23:00 +0200

    Внесены изменения

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> a;
     cout << "Введите второе число : ";
     cin >> b;
-    int sum = a + b;
-    cout << "Сумма : " << sum << endl;
+    int sum = a - b;
+    cout << "Разность : " << sum << endl;
 }
 
